e8d17c74f933fc8818b0257673bd679f8a5562c3
NguyenQuanBao <114391082+NguyenQuanBao@users.noreply.github.com>
2023-04-13 16:43:39 +0700

    Add files via upload

---
+++ src/HinhHoc/Shape.java
@@ -0,0 +1,38 @@
+package HinhHoc;
+
+public class Shape {
+    private String color;
+    private boolean filled;
+
+    public Shape() {
+        this.color = "Green";
+        this.filled = true;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "A Shape with " + "a color of " + color + " and " + filled;
+    }
+}
+
